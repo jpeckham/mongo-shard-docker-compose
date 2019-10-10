5ebf57def322f909a005aead3806551f78fc6765
James Peckham <jdpeckham@gmail.com>
2019-10-10 17:41:30 -0500

    actually do sharding on a collection

--- init.bat
+++ init.bat
@@ -4,3 +4,4 @@ docker-compose exec shard02a sh -c "mongo --port 27019 < /scripts/init-shard02.j
 ECHO "wait 20 seconds here then press any key to init router"
 pause
 docker-compose exec router sh -c "mongo < /scripts/init-router.js"
+
--- scripts/init-router.js
+++ scripts/init-router.js
@@ -5,3 +5,9 @@ sh.addShard("shard01/shard01c:27018")
 sh.addShard("shard02/shard02a:27019")
 sh.addShard("shard02/shard02b:27019")
 sh.addShard("shard02/shard02c:27019")
+
+sh.enableSharding("mydatabase")
+use mydatabase
+var shardKey = { "_id": "hashed" }
+db.customers.createIndex(shardKey)
+sh.shardCollection( "mydatabase.customers", shardKey)
